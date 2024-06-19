b0d7caf1560941070649cc683dc6973a286e1bb1
Abhijay <148853650+abhijaydas09@users.noreply.github.com>
2024-06-19 07:11:11 +0530

    Update terminal_commands.sh

--- k8s-configuration/terminal_commands.sh
+++ k8s-configuration/terminal_commands.sh
@@ -1,1 +1,15 @@
- minikube start 
+#starting up minikube cluster 
+minikube start 
+minikube status
+#applying all the files created in yaml 
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo-deployment.yaml 
+kubectl apply -f configmap.yaml
+kubectl apply -f mongo-express.yaml
+
+#if any pod or deployment not working 
+kubectl describe <file type> <file name>
+
+
+
+
